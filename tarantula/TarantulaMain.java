4954dfc1473e9b9034cc8370cea7d84183d63b51
Kevin Bi <kevinb22@hjk.cs.washington.edu>
2015-09-01 11:34:12 -0700

    "Added more comments to the program"

--- tarantula/TarantulaMain.java
+++ tarantula/TarantulaMain.java
@@ -3,27 +3,22 @@ package TarantulaCalculator;
 import java.util.Arrays;
 import java.io.File;
 import java.util.ArrayList;
-
-/**************************************************************************
- * Main for tarantula calculation, takes 2 command line arguments
- * arg[1] - compact-cov-matrix.json file
- * arg[2] - package.name of test program
- * examples of the args are in comments next to the args indexes below 
- **************************************************************************/
  
 public class TarantulaMain {
 	 
 	    public static void main(String[] args) throws ClassNotFoundException {
 	    	
 	        // create the Matrix Reader to get Test -- Stmt information
-	        File f = new File(args[0]); // "/abs/path/to/triangle-mvn-compact-cov-matrix.json"*
-		 reader = new covMatrixReader(f);
+	        File f = new File(args[0]); // example: "/homes/iws/kevinb22/Downloads/triangle-mvn-compact-cov-matrix.json"
+	        covMatrixReader reader = new covMatrixReader(f);
+	        
+	        // The test count is a Long in the Json file, must parse it to an Integer
 	        Long testCountLong = reader.getTestCount();
 	        String testCountString = testCountLong.toString();
 	        int testCountInt = Integer.parseInt(testCountString);
 	        
 	        // create Runner class to get the failing cases from JUnit tests
-	        Class testClass = Class.forName(args[1]); //"TarantulaCalculator.TriangleTest"
+	        Class testClass = Class.forName(args[1]); // example: "TarantulaCalculator.TriangleTest"
 	        Runner r = new Runner(testClass);
 	        r.determineFailCases();
 	        ArrayList<Integer> fails = r.getFailCases();
@@ -38,33 +33,40 @@ public class TarantulaMain {
 	         
 	        // set failing test cases -- [test]
 	        boolean[] F =new boolean[testCountInt];
-	        Arrays.fill(F, false);
+	        Arrays.fill(F, false); 
 	        for(Integer i : fails) {
-	            F[i - 1] = true;
+	            F[i - 1] = true; //true=failing test
 	        }
 	        calc.setF(F);
 	         
 	        // set live test cases -- [test]
+	        // by default all test cases are used
 	        boolean[] L = new boolean[testCountInt];
 	        Arrays.fill(L, true);
 	        calc.setL(L);
 	         
+	        // calc will perform all the calculations
 	        calc.compute();
 	         
+	        //display the results
 	        System.out.println("For program: " + reader.getFullName());
 	        displayConf(calc.getConfidence(), reader.getFirstLine());
 	        System.out.println("------------------------------------------------------------------");
 	        displaySusp(calc.getSuspiciousness(), reader.getFirstLine());
 	    }
-	    // display the confidence array for the program	     
+	    
+	    
+	    // displays the confidence per line of the program starting from the
+	    // first line of the program identified by tacoco
 	    public static void displayConf(double[] a, Long firstLine){
 	        System.out.println("Confidence Array");
 	        for(int i = 0; i < a.length; i++) {
 	            System.out.println("line " + (i+firstLine) + ": " + a[i]);
 	        }
 	    }
-	     
-	    // dispaly the susp. array for the program
+	    
+	    // displays the suspicion per line of the program starting from the
+	    // first line of the program identified by tacoco
 	    public static void displaySusp(double[] a, Long firstLine){
 	        System.out.println("Suspicion Array");
 	        for(int i = 0; i < a.length; i++) {
